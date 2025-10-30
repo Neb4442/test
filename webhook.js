1a0e52b76fc49b783368d131347d710d7877b48e
Neb4442 <blucas@esdacademy.eu>
2025-10-30 19:18:20 +0100

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,6 +1,4 @@
-const webhookURL = "https://discord.com/api/webhooks/1433463555168141443/bQkkQ7fF-z9FDdb52Uj45lVTImSVTbshs6xCfM-TZp0d4Nspss28fEHCQl9zalsFbTAa";
-
-fetch(webhookURL, {
+fetch("webhookURL", {
   method: "POST",
   headers: { "Content-Type": "application/json" },
   body: JSON.stringify({
